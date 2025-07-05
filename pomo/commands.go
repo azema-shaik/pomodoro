3a42656df0817a8a575c268d2fe38832940141e3
azema-shaik <skazema221@gmail.com>
2025-07-05 21:21:30 +0530

    implemented reset and status commands

--- pomo/commands.go
+++ pomo/commands.go
@@ -1,7 +1,11 @@
 package pomo
 
 import (
+	"database/sql"
 	"fmt"
+	"os"
+	"strings"
+	"text/tabwriter"
 	"time"
 
 	logging "github.com/azema-shaik/logger"
@@ -35,7 +39,6 @@ func Start(params map[string]any) *state {
 		timeNow.Format(timeFormat), userTimeLimit.Format(timeFormat),
 		reminderTime, breakTime)
 
-pomo:
 	for {
 		select {
 		case <-stateTimer.timer.C:
@@ -46,7 +49,7 @@ pomo:
 
 			<-time.After(time.Duration(breakTime) * time.Minute)
 			fmt.Printf("\033[38;5;10mYou have succesfully completed a pomodoro session🥳\033[0m\n")
-			break pomo
+			return stateTimer
 
 		case ticker := <-stateTimer.ticker.C:
 			timeLeft := userTimeLimit.Sub(ticker)
@@ -59,7 +62,87 @@ pomo:
 		}
 	}
 
-	return stateTimer
+}
+
+func Status(db *sql.DB, params map[string]any) {
+	session_name := params["session_name"].(string)
+	query := map[bool]string{true: `SELECT session_name, timestamp, time_duration, break_time, time_unit,reminder_time FROM pomo
+									WHERE username = :username`,
+		false: `SELECT session_name, timestamp, time_duration, break_time, time_unit,reminder_time FROM pomo
+									WHERE username = :username AND session_name = :session_name`}[session_name == "all"]
+	cmdLogger.Debug(fmt.Sprintf("status sql command: %s", query))
+
+	countRows := 0
+
+	rows := fetchRows(db, query, session_name)
+
+	var totalTime int
+	for rows.Next() {
+		var unit, session_name string
+		var session_duration, session_break, session_reminder int
+		var timestamp time.Time
+
+		err := rows.Scan(&session_name, &timestamp, &session_duration, &session_break, &unit, session_reminder)
+		if err != nil {
+			cmdLogger.Error(fmt.Sprintf("error when fetching rows. err: %s", err.Error()))
+			fmt.Println("\033[38;5;9mIssue when fetching errors.Consult logs.\033[0m")
+			os.Exit(1)
+		}
+
+		fmt.Printf("\033[38;5;10mSession Name: %s, Session Time: %s\nSession Duration: %d %s, Session Break: %d minute(s), Session Reminder: %d minute(s)\033[0m",
+			session_name, utcToIst(timestamp), session_duration, unit, session_break, session_reminder)
+		fmt.Println(strings.Repeat("=", 50))
+
+		totalTime += map[string]int{
+			"hour":   session_duration * 60,
+			"minute": session_duration}[unit]
+
+		countRows += 1
+
+	}
+
+	if countRows == 0 {
+		fmt.Println("\033[38;5;14mNo records found.\033[0m")
+	} else {
+		duration := map[bool]time.Duration{
+			true:  time.Hour,
+			false: time.Minute}[totalTime > 60]
+		fmt.Printf("\033[38;5;14mBe proud you have completed %s\033[0m\n", time.Duration(totalTime)*duration)
+	}
+}
+
+func Reset(db *sql.DB, params map[string]any) {
+	session_name := params["session_name"].(string)
+	query := map[bool]string{
+		true:  `DELETE FROM pomo WHERE username = :username RETURNING session_name, timestamp, time_duration, time_unit`,
+		false: `DELETE FROM pomo WHERE username = :username AND session_name = :name RETURNING session_name, timestamp, time_duration, time_unit`,
+	}[session_name == "all"]
+
+	rows := fetchRows(db, query, session_name)
+	w := tabwriter.NewWriter(os.Stdout, 5, 5, 5, ' ', tabwriter.AlignRight|tabwriter.Debug)
+	defer w.Flush()
+
+	fmt.Fprintln(w, "session_name\ttimestamp\ttime_duration")
+	rowCount := 0
+	for rows.Next() {
+		var session_name, time_unit string
+		var time_duration int
+		var timestamp time.Time
+
+		err := rows.Scan(&session_name, &timestamp, &time_duration, &time_unit)
+		if err != nil {
+			cmdLogger.Error(fmt.Sprintf("error when scaning rows: %s", err.Error()))
+			fmt.Println("\033[38;5;9mError when trying initalizing app. Consult logs.\033[0m")
+			os.Exit(1)
+		}
+
+		fmt.Fprintf(w, "%s\n%s\t%d %s\n", session_name, utcToIst(timestamp), time_duration, time_unit)
+		rowCount += 1
+	}
+
+	if rowCount == 0 {
+		fmt.Println("\033[38;5;14mNo rows found.\033[0m")
+	}
 
 }
 
--- pomo/utils.go
+++ pomo/utils.go
@@ -1,9 +1,49 @@
 package pomo
 
-func dictLike(dict map[any]any, key, elseCond any) any {
-	ans, ok := dict[key]
-	if !ok {
-		ans = elseCond
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/azema-shaik/logger"
+)
+
+var utilLogger *logger.Logger
+var timeZone *time.Location
+var timeFormat string = "Monday 02-01-2006 03:04:05 PM MST"
+
+func utcToIst(timestamp time.Time) string {
+	return timestamp.In(timeZone).Format(timeFormat)
+}
+
+func fetchRows(db *sql.DB, query, session_name string) *sql.Rows {
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		cmdLogger.Error(fmt.Sprintf("error when trying to prepare query, %s", err.Error()))
+		fmt.Println("\033[38;5;9mIssue when trying to initalize connection to the app. Consult logs.\033[0m")
+		os.Exit(1)
 	}
-	return ans
+
+	rows, err := stmt.Query(sql.Named("username", os.Getenv("USERNAME")),
+		sql.Named("session_name", session_name))
+	if err != nil {
+		cmdLogger.Error(fmt.Sprintf("error when trying to query db. err: %s", err.Error()))
+		fmt.Println("\033[38;5;9mError when trying to initialize connection to the app. Consult logs.\033[0m")
+		os.Exit(1)
+	}
+	return rows
+}
+
+func init() {
+	utilLogger = NewLogger("utils")
+	timeZone, err := time.LoadLocation("Asia/Kolkata")
+	if err != nil {
+		utilLogger.Error(fmt.Sprintf("error initalizing timezone defaulting to utc. err: %s", err))
+	}
+
+	timeZone = map[bool]*time.Location{
+		true:  time.UTC,
+		false: timeZone}[err != nil]
+
 }
